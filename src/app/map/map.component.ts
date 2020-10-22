d67c9e832a028a17f0558ea75275c4ad9b16257c
Sergi <saaroca@gmail.com>
2020-10-22 14:44:29 +0200

    Sidebar funcionant PORFIN!

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,11 +1,12 @@
 import { AfterViewInit, Component, Pipe } from '@angular/core';
 import * as L from 'leaflet';
 import { TPO, PuntsInteres, ParcNou, Icona } from '../../assets/js/sample-geojson.js';
+import './js/leaflet-sidebar.js'
 
 
 @Component({
   selector: 'app-map',
-  templateUrl: './map.component.html', 
+  templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
 
@@ -98,10 +99,44 @@ export class MapComponent {
 
       onEachFeature: this.onEachFeature
     }).addTo(this.map);
-    
-    L.control.sidebar('sidebar', {
+
+    var sidebar = L.control.sidebar('sidebar', {
+      closeButton: true,
       position: 'left'
-    }).addTo(this.map);
+  });
+  this.map.addControl(sidebar);
+
+  setTimeout(function () {
+      sidebar.show();
+  }, 500);
+
+  var marker = L.marker([51.2, 7]).addTo(this.map).on('click', function () {
+      sidebar.toggle();
+  });
+
+  this.map.on('click', function () {
+      sidebar.hide();
+  })
+
+  sidebar.on('show', function () {
+      console.log('Sidebar will be visible.');
+  });
+
+  sidebar.on('shown', function () {
+      console.log('Sidebar is visible.');
+  });
+
+  sidebar.on('hide', function () {
+      console.log('Sidebar will be hidden.');
+  });
+
+  sidebar.on('hidden', function () {
+      console.log('Sidebar is hidden.');
+  });
+
+  L.DomEvent.on(sidebar.getCloseButton(), 'click', function () {
+      console.log('Close button clicked.');
+  });
   }
 
 };
